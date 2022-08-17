6fe820bfebd5203b4f16ad4388fcaa2b6e5feaa0
yuh8 <yhytxwd@gmail.com>
2022-08-17 17:49:51 +0800

    embed layernorm after dropout

--- src/embed_utils.py
+++ src/embed_utils.py
@@ -9,12 +9,7 @@ class GraphEmbed(tf.keras.layers.Layer):
         self.d_model = d_model
         self.kernel_width = kernel_width
         self.pad = tf.keras.layers.ZeroPadding2D(padding=[0, 1])
-        self.conv = tf.keras.layers.Conv2D(self.d_model,
-                                           kernel_size=[1, kernel_width],
-                                           activation='relu',
-                                           padding='same')
         self.layernorm = tf.keras.layers.LayerNormalization(epsilon=1e-6)
-        self.layernorm_1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
         self.dropout = tf.keras.layers.Dropout(rate)
 
     def build(self, input_shape):
@@ -33,11 +28,9 @@ class GraphEmbed(tf.keras.layers.Layer):
             x = self.pad(x)
         # [batch_size, num_atoms, d_model]
         x = self.embed(x)
-        x = self.layernorm(x)
-        x = self.conv(x)
         x = tf.reduce_sum(x, axis=1)
-        x = self.layernorm_1(x)
         x = self.dropout(x, training)
+        x = self.layernorm(x)
         return x
 
 
